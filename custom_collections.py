6eaf651ee8530e9184a032bbbda4bfadb8084a26
Jose <nino.capati@gmail.com>
2024-02-25 20:46:02 +0800

    cleanup

--- custom_collections.py
+++ custom_collections.py
@@ -1,16 +1,10 @@
 from pyjan26.registry import register_custom_collections
 
-def custom_collection_function1(collections):
-    # Logic to generate collection 1
-    return {'collection1': [{'metadata': {}, 'content': 'Content 1'}, {'metadata': {}, 'content': 'Content 2'}]}
-
-def custom_collection_function2(collections):
-    #print(collections)
-    # Logic to generate collection 2
-    return {'collection2': [{'metadata': {}, 'title': 'Custom Collections 1'}, {'metadata': {}, 'title': 'Custom Collections 2'}]}
+def tag_list(collections):
+    #pprint.pprint(collections)
+    return {'tags': [{ 'tag': 'javascript'}, {'tag': 'python' }] }
 
 
 register_custom_collections([
-    custom_collection_function1,
-    custom_collection_function2
+    tag_list
 ])
